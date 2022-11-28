fba0271a9fea8b1cd5ccd488c14aca77d82108bd
Sebastian <sebastianbentley123@gmail.com>
2022-11-28 11:31:16 +0100

    hello endpoint

--- src/main/java/dk/f2js/ratingproducer/producer/ProducerController.java
+++ src/main/java/dk/f2js/ratingproducer/producer/ProducerController.java
@@ -1,10 +1,7 @@
 package dk.f2js.ratingproducer.producer;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/kafka")
@@ -18,5 +15,11 @@ public class ProducerController {
     {
         service.sendRating(rating);
         return "Message published: " + rating;
+    }
+
+    @GetMapping(value = "/hello")
+    public String hello()
+    {
+        return "Hello from producer";
     }
 }
